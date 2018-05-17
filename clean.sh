21d329abc07e2fb3f58a353357316ad2e8e963fb
Andrew Marcuse <andrew@moven.com>
2018-05-17 11:00:45 -0400

    ability to make unsigned .IPA

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+rm -rf MSIReader.xarchive
+rm MSIReader.ipa
---
+++ make_unsigned.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+xcodebuild clean CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO archive -scheme MSIReader -configuration MSIReader -archivePath MSIReader.xcarchive
+
+xcodebuild -exportArchive -archivePath ./MSIReader.xcarchive -exportOptionsPlist export.plist -exportPath .
