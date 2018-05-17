9cc109bc863e3a1ae351dfb258c6e0e8502cd7ed
Andrew Marcuse <andrew@moven.com>
2018-05-17 11:02:49 -0400

    use Apple-generated export plist

--- make_unsigned.sh
+++ make_unsigned.sh
@@ -3,4 +3,4 @@
 
 xcodebuild clean CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO archive -scheme MSIReader -configuration MSIReader -archivePath MSIReader.xcarchive
 
-xcodebuild -exportArchive -archivePath ./MSIReader.xcarchive -exportOptionsPlist export.plist -exportPath .
+xcodebuild -exportArchive -archivePath ./MSIReader.xcarchive -exportOptionsPlist ExportOptions.plist -exportPath .
