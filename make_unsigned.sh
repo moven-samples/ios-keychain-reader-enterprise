2d21b11998e19ae151de42c3669d87fe51bb6c11
Andrew Marcuse <andrew@moven.com>
2018-05-17 22:43:19 -0400

    working build script

--- make_unsigned.sh
+++ make_unsigned.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-
 xcodebuild clean CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO archive -scheme MSIReader -configuration MSIReader -archivePath MSIReader.xcarchive
 
 xcodebuild -exportArchive -archivePath ./MSIReader.xcarchive -exportOptionsPlist ExportOptions.plist -exportPath .
+
+S3_NAME=s3://moven-app-builds-bca/MSIReader-unsigned-$(date -u +%Y%m%d-%H%M).ipa
+
+aws --profile=am s3 cp MSIReader.ipa $S3_NAME
+
+echo "INFO: IPA is uploaded as ${S3_NAME}"
